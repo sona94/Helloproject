77c839f4212f59d7edd42445e3de6e2cbbc6524d
sona94 <37668604+sona94@users.noreply.github.com>
2020-07-15 14:08:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("second line of the code");
+            Console.WriteLine("third line of the code");
+            // This is the write method to print the code.
+           /* Console.Write("Hello ");
+            Console.Write("Class"); */
+
+        }
+    }
+}
